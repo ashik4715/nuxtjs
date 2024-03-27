b5a4bc60e971389e456a36ed5836d92b1475004e
ashik4715 <ashikurjhalak@gmail.com>
2024-03-27 02:28:07 +0000

    Hide tag text

--- app.vue
+++ app.vue
@@ -161,7 +161,7 @@
       <div class="text-center">
         <div class="flex items-center justify-center space-x-8">
           <a href="https://dribbble.com/amitbwas007" target="_blank" rel="noopener noreferrer"
-            class="invisible-text w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+            class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
             <i class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500">
               <span class="sr-only">Dribbble</span>
               <svg class="w-16 h-16" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
@@ -169,11 +169,12 @@
                   d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
                   clip-rule="evenodd"></path>
               </svg>
-            </i>Follow me on Dribbble
+            </i>
+            <p><span class="hidden-text">Follow me on Dribbble</span></p>
           </a>
 
           <a href="https://www.facebook.com/ajholok" target="_blank" rel="noopener noreferrer"
-            class="invisible-text w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+            class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
             <i class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500">
               <span class="sr-only">Facebook</span>
               <svg class="w-16 h-16" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
@@ -182,11 +183,11 @@
                   clip-rule="evenodd"></path>
               </svg>
             </i>
-            Follow me on Facebook
+            <p><span class="hidden-text">Follow me on Facebook</span></p>
           </a>
 
           <a href="https://github.com/ashik4715" target="_blank" rel="noopener noreferrer"
-            class="invisible-text w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+            class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
             <i class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500">
               <span class="sr-only">Github</span>
               <svg class="w-16 h-16" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
@@ -194,27 +195,30 @@
                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                   clip-rule="evenodd"></path>
               </svg>
-            </i>Follow me on Github
+            </i>
+            <p><span class="hidden-text">Follow me on Github</span></p>
           </a>
 
           <a href="mailto:ashikurjhalak@gmail.com" rel="noopener noreferrer"
-            class="invisible-text w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+            class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
             <i class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500">
               <svg class="w-16 h-16" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                 <path
                   d="M12.042 23.648c-7.813 0-12.042-4.876-12.042-11.171 0-6.727 4.762-12.125 13.276-12.125 6.214 0 10.724 4.038 10.724 9.601 0 8.712-10.33 11.012-9.812 6.042-.71 1.108-1.854 2.354-4.053 2.354-2.516 0-4.08-1.842-4.08-4.807 0-4.444 2.921-8.199 6.379-8.199 1.659 0 2.8.876 3.277 2.221l.464-1.632h2.338c-.244.832-2.321 8.527-2.321 8.527-.648 2.666 1.35 2.713 3.122 1.297 3.329-2.58 3.501-9.327-.998-12.141-4.821-2.891-15.795-1.102-15.795 8.693 0 5.611 3.95 9.381 9.829 9.381 3.436 0 5.542-.93 7.295-1.948l1.177 1.698c-1.711.966-4.461 2.209-8.78 2.209zm-2.344-14.305c-.715 1.34-1.177 3.076-1.177 4.424 0 3.61 3.522 3.633 5.252.239.712-1.394 1.171-3.171 1.171-4.529 0-2.917-3.495-3.434-5.246-.134z" />
               </svg>
-            </i>Contact us by email
+            </i>
+            <p><span class="hidden-text">Contact me by email</span></p>
           </a>
 
           <a href="https://www.instagram.com/ashikur4715/" target="_blank" rel="noopener noreferrer"
-            class="invisible-text w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+            class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
             <i class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500">
               <svg class="w-16 h-16" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                 <path
                   d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z" />
               </svg>
-            </i>Follow me on Instagram
+            </i>
+            <p><span class="hidden-text">Follow me on instagram</span></p>
           </a>
         </div>
       </div>
@@ -281,7 +285,7 @@ nav.scrolled {
   border-bottom: 0px;
 }
 
-.invisible-text {
+.hidden-text {
   display: none;
 }
 </style>
